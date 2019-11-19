1e10dc40dc3a34239eea3422df4e5149fe55c6bc
Eryx <evorui@gmail.com>
2019-11-19 19:55:51 +0800

    [NEW] release 0.1.2. archive old SKV APIs to v0.1, and move the new SKO APIs to v0.2

--- const.go
+++ const.go
@@ -15,16 +15,11 @@
 package kvgo
 
 const (
-	Version = "0.1.1"
+	Version = "0.1.2"
 
 	ns_meta     uint8 = 4
 	ns_ttl      uint8 = 5
 	ns_kv       uint8 = 8
-	ns_sko_sys  uint8 = 16
-	ns_sko_meta uint8 = 17
-	ns_sko_data uint8 = 18
-	ns_sko_log  uint8 = 19
-	ns_sko_ttl  uint8 = 20
 	ns_prog_ttl uint8 = 32
 	ns_prog_def uint8 = 36
 	ns_prog_cut uint8 = 63
